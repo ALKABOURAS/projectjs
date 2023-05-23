c08ecbe4180556db204f559a9840074f7dd625da
ALKABOURAS <66868213+ALKABOURAS@users.noreply.github.com>
2023-05-24 00:39:13 +0300

    / done and teams nearly done

--- index.js
+++ index.js
@@ -36,23 +36,23 @@ app.get('/', (req, res) => {
 } );
 app.get('/teams', (req, res) => {
     res.render('teams', {title: 'Teams', css: 'teams.css',
-        team_list: [{'stad_img':'toumpa-small.svg','team_img':"norths-small",'team_bg':'#1E1E1E','team_name':'Salonica Norths'},
-            {'stad_img':'narcos-small.svg','team_img':"reds-small",'team_bg':'#D31313','team_name':'Noor1 Reds'},
-            {'stad_img': 'votanikos-small.svg','team_img':"greens-small",'team_bg':'#24A83C','team_name':'Green Last Believers'},
-            {'stad_img': 'propap-small.svg','team_img':"crabs-small",'team_bg':'rgba(255, 235, 0, 0.96)','team_name':'New Philla Crabs'},
-            {'stad_img': 'patras-small.svg','team_img':"winners-small",'team_bg':'#980100','team_name':'Patras Winners'},
-            {'stad_img': 'elders-small.svg','team_img':"selders-small",'team_bg':'#007AFF','team_name':'Salonica Elders'},
-            {'stad_img': 'morias-small.svg','team_img':"traiders-small",'team_bg':'#215E97','team_name':'Triple City Raiders'},
-            {'stad_img':'volos-small.svg','team_img':"avolos-small",'team_bg':'#C2D1D9','team_name':'Volos Angels'}],
+        team_list: [{'stad_img':'toumpa-small.svg','team_img':"norths-small",'team_bg':'#1E1E1E','team_name':'Salonica Norths', 'logos_team_name':'norths'},
+            {'stad_img':'narcos-small.svg','team_img':"reds-small",'team_bg':'#D31313','team_name':'Noor1 Reds', 'logos_team_name':'reds'},
+            {'stad_img': 'votanikos-small.svg','team_img':"greens-small",'team_bg':'#24A83C','team_name':'Green Last Believers', 'logos_team_name':'greens'},
+            {'stad_img': 'propap-small.svg','team_img':"crabs-small",'team_bg':'#BAAC13','team_name':'New Philla Crabs', 'logos_team_name':'crabs'},
+            {'stad_img': 'patras-small.svg','team_img':"winners-small",'team_bg':'#980100','team_name':'Patras Winners', 'logos_team_name':'winners'},
+            {'stad_img': 'elders-small.svg','team_img':"elders-small",'team_bg':'#007AFF','team_name':'Salonica Elders', 'logos_team_name':'elders'},
+            {'stad_img': 'morias-small.svg','team_img':"raiders-small",'team_bg':'#215E97','team_name':'Triple City Raiders', 'logos_team_name':'raiders'},
+            {'stad_img':'volos-small.svg','team_img':"angels-small",'team_bg':'#C2D1D9','team_name':'Volos Angels', 'logos_team_name':'angels'}],
         logos:[
-            {'logo_id': 'norths_logo','logo_src': 'norths-logo.svg'},
-            {'logo_id': 'reds_logo','logo_src': 'reds-logo.svg'},
-            {'logo_id': 'greens_logo','logo_src': 'greens-logo.svg'},
-            {'logo_id': 'crabs_logo','logo_src': 'crabs-logo.svg'},
-            {'logo_id': 'winners_logo','logo_src': 'winners-logo.svg'},
-            {'logo_id': 'elders_logo','logo_src': 'elders-logo.svg'},
-            {'logo_id': 'raiders_logo','logo_src': 'raiders-logo.svg'},
-            {'logo_id': 'angels_logo','logo_src': 'angels-logo.svg'}
+            {'logo_id': 'norths_logo','logo_src': 'norths-logo.svg' ,'team_name':'norths'},
+            {'logo_id': 'reds_logo','logo_src': 'reds-logo.svg','team_name':'reds'},
+            {'logo_id': 'greens_logo','logo_src': 'greens-logo.svg','team_name':'greens'},
+            {'logo_id': 'crabs_logo','logo_src': 'crabs-logo.svg','team_name':'crabs'},
+            {'logo_id': 'winners_logo','logo_src': 'winners-logo.svg','team_name':'winners'},
+            {'logo_id': 'elders_logo','logo_src': 'elders-logo.svg','team_name':'elders'},
+            {'logo_id': 'raiders_logo','logo_src': 'raiders-logo.svg','team_name':'raiders'},
+            {'logo_id': 'angels_logo','logo_src': 'angels-logo.svg','team_name':'angels'}
         ],
         infos:[
             {'info_id': 'location','info_src': 'location.svg','info_text': 'Μεσογείων 174, 151 25 Μαρούσι'},
@@ -70,14 +70,14 @@ app.get('/teams', (req, res) => {
 app.get('/schedule', (req, res) => {
     res.render('schedule', {title: 'Schedule', css: 'schedule.css',
         logos:[
-            {'logo_id': 'norths_logo','logo_src': 'norths-logo.svg'},
-            {'logo_id': 'reds_logo','logo_src': 'reds-logo.svg'},
-            {'logo_id': 'greens_logo','logo_src': 'greens-logo.svg'},
-            {'logo_id': 'crabs_logo','logo_src': 'crabs-logo.svg'},
-            {'logo_id': 'winners_logo','logo_src': 'winners-logo.svg'},
-            {'logo_id': 'elders_logo','logo_src': 'elders-logo.svg'},
-            {'logo_id': 'raiders_logo','logo_src': 'raiders-logo.svg'},
-            {'logo_id': 'angels_logo','logo_src': 'angels-logo.svg'}
+            {'logo_id': 'norths_logo','logo_src': 'norths-logo.svg' ,'team_name':'norths'},
+            {'logo_id': 'reds_logo','logo_src': 'reds-logo.svg','team_name':'reds'},
+            {'logo_id': 'greens_logo','logo_src': 'greens-logo.svg','team_name':'greens'},
+            {'logo_id': 'crabs_logo','logo_src': 'crabs-logo.svg','team_name':'crabs'},
+            {'logo_id': 'winners_logo','logo_src': 'winners-logo.svg','team_name':'winners'},
+            {'logo_id': 'elders_logo','logo_src': 'elders-logo.svg','team_name':'elders'},
+            {'logo_id': 'raiders_logo','logo_src': 'raiders-logo.svg','team_name':'raiders'},
+            {'logo_id': 'angels_logo','logo_src': 'angels-logo.svg','team_name':'angels'}
         ],
         infos:[
             {'info_id': 'location','info_src': 'location.svg','info_text': 'Μεσογείων 174, 151 25 Μαρούσι'},
@@ -100,6 +100,45 @@ app.get('/schedule', (req, res) => {
                 'info_id2': 'location','info_src2': 'location-black.svg',
                 'info_id1': 'phone_number','info_src1': 'clock-dark.svg',
                 'match_location':'Narcos Arena'}]} )});
+
+team_dets=[[{ 'stad_img':'toumpa.svg','team_img':"norths-small",'team_bg':'#1E1E1E','team_name':'Salonica Norths'}],
+    [{'stad_img':'narcos.svg','team_img':"reds-small",'team_bg':'#D31313','team_name':'Noor1 Reds'}],
+    [{'stad_img': 'botanikos.svg','team_img':"greens-small",'team_bg':'#24A83C','team_name':'Green Last Believers'},
+    ],[{'stad_img': 'propap.svg','team_img':"crabs-small",'team_bg':'#BAAC13','team_name':'New Philla Crabs'},
+    ],[{'stad_img': 'patras.svg','team_img':"winners-small",'team_bg':'#980100','team_name':'Patras Winners'},
+    ],[{'stad_img': 'elders.svg','team_img':"elders-small",'team_bg':'#007AFF','team_name':'Salonica Elders'},
+    ],[{'stad_img': 'morias.svg','team_img':"raiders-small",'team_bg':'#215E97','team_name':'Triple City Raiders'},
+    ],[{'stad_img':'volos.svg','team_img':"angels-small",'team_bg':'#C2D1D9','team_name':'Volos Angels'}
+]]
+names = ['norths','reds','greens','crabs','winners','elders','raiders','angels']
+for (let i = 0; i < team_dets.length; i++) {
+    app.get('/teams/'+names[i], (req, res) => {
+    res.render('team-page', {title: 'Team', css: 'team-banner.css',
+        logos:[
+            {'logo_id': 'norths_logo','logo_src': 'norths-logo.svg' ,'team_name':'norths'},
+            {'logo_id': 'reds_logo','logo_src': 'reds-logo.svg','team_name':'reds'},
+            {'logo_id': 'greens_logo','logo_src': 'greens-logo.svg','team_name':'greens'},
+            {'logo_id': 'crabs_logo','logo_src': 'crabs-logo.svg','team_name':'crabs'},
+            {'logo_id': 'winners_logo','logo_src': 'winners-logo.svg','team_name':'winners'},
+            {'logo_id': 'elders_logo','logo_src': 'elders-logo.svg','team_name':'elders'},
+            {'logo_id': 'raiders_logo','logo_src': 'raiders-logo.svg','team_name':'raiders'},
+            {'logo_id': 'angels_logo','logo_src': 'angels-logo.svg','team_name':'angels'}
+        ],
+            infos:[
+                {'info_id': 'location','info_src': 'location.svg','info_text': 'Μεσογείων 174, 151 25 Μαρούσι'},
+                {'info_id': 'phone_number','info_src': 'phone.svg','info_text': '+302310954050'},
+                {'info_id': 'email','info_src': 'email.svg','info_text': 'info@ultraleague.gr'}
+            ],
+            navbar:[
+                {'navbar_text': 'Ομάδες', 'button_href': '/teams'},
+                {'navbar_text': 'Πρόγραμμα', 'button_href': '/schedule'},
+                {'navbar_text': 'Βαθμολογία', 'button_href': '/standings'},
+                {'navbar_text': 'Επικοινωνία', 'button_href': '/contact'},
+                {'navbar_text': 'About', 'button_href': '/about'}
+            ],
+            team_dets: team_dets[i]
+
+        } )})};
 
 
 app.listen(8080, () => {
